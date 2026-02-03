b789b824ca1649b976cde626578bef0569f563b4
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-02-03 14:23:05 +0530

    Deleted

---
+++ redis.sh
@@ -0,0 +1,20 @@
+source ./common.sh
+
+app-name=redis
+check_root
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
+
+print_total_time
